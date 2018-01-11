911bfc7bf683ba72982c3b888df23d767de8912d
Axel Magnuson <axelmagn@google.com>
2018-01-11 14:31:40 -0800

    WIP Create empty HarnessManager and DockerShellClient

---
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/harness/DockerShellClient.java
@@ -0,0 +1,16 @@
+package org.apache.beam.runners.flink.harness;
+
+/**
+ * A docker client that operates by executing shell commands to interact with docker.
+ */
+public class DockerShellClient {
+    // TODO: isAvailable
+
+    // TODO: import
+
+    // TODO: create
+
+    // TODO: start
+
+    // TODO: stop
+}
---
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/harness/HarnessManager.java
@@ -0,0 +1,8 @@
+package org.apache.beam.runners.flink.harness;
+
+/**
+ * Manage long-lived Beam SDK harnesses on behalf of shorter lived flink operations.
+ */
+public class HarnessManager {
+    // TODO: runProofOfConcept
+}
