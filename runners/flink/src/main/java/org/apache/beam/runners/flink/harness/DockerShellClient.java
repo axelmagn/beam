e15733876a521c02bb5bcc6471fe8b0eedd9430e
Axel Magnuson <axelmagn@google.com>
2018-01-11 15:06:19 -0800

    WIP write DockerShellClient.isAvailable

--- runners/flink/src/main/java/org/apache/beam/runners/flink/harness/DockerShellClient.java
+++ runners/flink/src/main/java/org/apache/beam/runners/flink/harness/DockerShellClient.java
@@ -1,10 +1,31 @@
 package org.apache.beam.runners.flink.harness;
 
+import com.google.common.collect.ImmutableList;
+
+import java.io.IOException;
+
 /**
  * A docker client that operates by executing shell commands to interact with docker.
  */
 public class DockerShellClient {
-    // TODO: isAvailable
+
+    // set as a variable in case we need to refactor to /usr/bin/docker or $(which docker) later.
+    private static final String DOCKER_CMD = "docker";
+
+    private final Runtime runtime;
+
+    public DockerShellClient() {
+        this.runtime = Runtime.getRuntime();
+    }
+
+    public DockerShellClient(Runtime runtime) {
+        this.runtime = runtime;
+    }
+
+    public boolean isAvailable() throws IOException, InterruptedException {
+        Process proc = runCmd(DOCKER_CMD, "version");
+        return proc.waitFor() == 0;
+    }
 
     // TODO: import
 
@@ -13,4 +34,14 @@ public class DockerShellClient {
     // TODO: start
 
     // TODO: stop
+
+    /**
+     * Execute a command in bash
+     * @param command command to execute
+     * @return the resulting process
+     */
+    private Process runCmd(String... command) throws IOException {
+        // TODO: explicitly set envp and working dir from config?
+        return runtime.exec(command);
+    }
 }
