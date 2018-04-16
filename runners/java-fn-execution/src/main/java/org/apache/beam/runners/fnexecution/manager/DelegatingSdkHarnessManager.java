4f1ff904d69cdc0a10bf3651290136ea62c3b841
Axel Magnuson <axelmagn@gmail.com>
2018-04-16 13:10:36 -0700

    WIP grab code in progress from `hacking-job-server`

---
+++ runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/manager/DelegatingSdkHarnessManager.java
@@ -0,0 +1,89 @@
+package org.apache.beam.runners.fnexecution.manager;
+
+import com.google.auto.value.AutoValue;
+import org.apache.beam.model.fnexecution.v1.ProvisionApi;
+import org.apache.beam.model.pipeline.v1.RunnerApi;
+import org.apache.beam.runners.fnexecution.GrpcFnServer;
+import org.apache.beam.runners.fnexecution.artifact.ArtifactRetrievalService;
+import org.apache.beam.runners.fnexecution.artifact.ArtifactSource;
+import org.apache.beam.runners.fnexecution.control.ControlClientPool;
+import org.apache.beam.runners.fnexecution.control.FnApiControlClientPoolService;
+import org.apache.beam.runners.fnexecution.environment.EnvironmentManager;
+import org.apache.beam.runners.fnexecution.environment.RemoteEnvironment;
+import org.apache.beam.runners.fnexecution.logging.GrpcLoggingService;
+import org.apache.beam.runners.fnexecution.manager.EnvironmentSession;
+import org.apache.beam.runners.fnexecution.manager.SdkHarnessManager;
+import org.apache.beam.runners.fnexecution.provisioning.StaticGrpcProvisionService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Manages SDK harness containers and job resources on behalf of
+ * worker operators.  It is responsible for initializing and storing
+ * job resources for use.  It delegates instantiation tasks to the
+ * factory classes passed into it.
+ *
+ * @TODO(axelmagn): extend to clean up unused containers and resources
+ */
+public class DelegatingSdkHarnessManager implements SdkHarnessManager {
+
+  /**
+   * Resources bound to the lifetime of a job.
+   * @TODO(axelmagn): identify environment resources
+   */
+  @AutoValue
+  abstract static class JobResources {
+    abstract GrpcFnServer<GrpcLoggingService> loggingServer();
+    abstract GrpcFnServer<StaticGrpcProvisionService> provisionServer();
+    abstract GrpcFnServer<ArtifactRetrievalService> retrievalServer();
+  }
+
+  /**
+   * Resources bound to the lifetime of a container environment.
+   * @TODO(axelmagn): identify environment resources
+   */
+  @AutoValue
+  abstract static class EnvironmentResources {
+    abstract ControlClientPool controlClientPool();
+    abstract RemoteEnvironment remoteEnvironment();
+    abstract GrpcFnServer<FnApiControlClientPoolService> controlServer();
+  }
+
+  // TODO(axelmagn): figure out if this needs to be threadsafe
+  // key: ProvisionApi.ProvisionInfo.job_id
+  private final Map<String, JobResources> jobResourcesCache;
+
+  // TODO(axelmagn): figure out if this needs to be threadsafe
+  // key: RunnerApi.Environment.url
+  private final Map<String, EnvironmentResources> environmentResourcesCache;
+
+  // unbounded lifetime resources
+
+
+  public DelegatingSdkHarnessManager() {
+    jobResourcesCache = new HashMap<>();
+    environmentResourcesCache = new HashMap<>();
+  }
+
+  @Override
+  public EnvironmentSession getSession(
+      ProvisionApi.ProvisionInfo jobInfo,
+      RunnerApi.Environment environment,
+      ArtifactSource artifactSource) throws Exception {
+    // TODO(axelmagn): create session from resources
+    JobResources jobResources = getOrCreateJobResources(jobInfo);
+    EnvironmentResources environmentResources = getOrCreateEnvironmentResources(environment);
+    return null;
+  }
+
+  private JobResources getOrCreateJobResources(ProvisionApi.ProvisionInfo jobInfo) {
+    // TODO(axelmagn)
+    return null;
+  }
+
+  private EnvironmentResources getOrCreateEnvironmentResources(RunnerApi.Environment environment) {
+    // TODO(axelmagn)
+    return null;
+  }
+}
---
+++ runners/java-fn-execution/src/main/java/org/apache/beam/runners/fnexecution/manager/EnvironmentSession.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.beam.runners.fnexecution.manager;
+
+import org.apache.beam.model.pipeline.v1.Endpoints.ApiServiceDescriptor;
+import org.apache.beam.model.pipeline.v1.RunnerApi.Environment;
+import org.apache.beam.runners.fnexecution.artifact.ArtifactSource;
+import org.apache.beam.runners.fnexecution.control.SdkHarnessClient;
+import org.apache.beam.runners.fnexecution.state.StateDelegator;
+
+/**
+ * A handle to an {@link Environment} managed by a {@link SdkHarnessManager}.
+ * Closing the session indicates to the session's {@link SdkHarnessManager}
+ * that it can no longer use session resources such as its
+ * {@link ArtifactSource}.
+ *
+ * // TODO(axelmagn): refactor ArtifactSource to follow StateDelegator pattern instead
+ */
+public interface EnvironmentSession extends AutoCloseable {
+
+  /**
+   * Get the environment that the session uses.
+   */
+  Environment getEnvironment();
+
+  /**
+   * Get the ArtifactSource that the session uses.
+   */
+  ArtifactSource getArtifactSource();
+
+  /**
+   * Get an {@link SdkHarnessClient} that can communicate with an instance of the environment.
+   */
+  SdkHarnessClient getClient();
+
+  /**
+   * Get a {@link StateDelegator} with which to register a StateRequestHandler.
+   */
+  StateDelegator getStateDelegator();
+
+  /**
+   * Get a {@link ApiServiceDescriptor} for the data service.
+   */
+  ApiServiceDescriptor getDataServiceDescriptor();
+
+  /**
+   * Get a {@link ApiServiceDescriptor} for the state service.
+   */
+  ApiServiceDescriptor getStateServiceDescriptor();
+}
